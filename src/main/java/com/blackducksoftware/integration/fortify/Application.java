e47425d77db9b6dbd5956590d6b377ab8a010152
Senthil Murugan Manikantan <smanikantan@blackducksoftware.com>
2017-04-27 15:55:57 -0400

    Removed the command line runner in Application

--- src/main/java/com/blackducksoftware/integration/fortify/Application.java
+++ src/main/java/com/blackducksoftware/integration/fortify/Application.java
@@ -11,16 +11,11 @@
  */
 package com.blackducksoftware.integration.fortify;
 
-import java.util.Arrays;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
@@ -28,20 +23,5 @@ import org.springframework.context.annotation.Bean;
 public class Application {
     public static void main(String args[]) {
         SpringApplication.run(Application.class);
-    }
-
-    @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-        return args -> {
-
-            System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-            String[] beanNames = ctx.getBeanDefinitionNames();
-            Arrays.sort(beanNames);
-            for (String beanName : beanNames) {
-                System.out.println(beanName);
-            }
-
-        };
     }
 }
