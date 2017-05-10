2016f98083291640dc51dc5f6224a3fdd1e94aca
Senthil Murugan Manikantan <smanikantan@blackducksoftware.com>
2017-05-10 10:22:24 -0400

    Modified the application properties keys to have same case
    Modified the application properties keys to have same case

--- src/main/java/com/blackducksoftware/integration/fortify/service/FortifyFileTokenApi.java
+++ src/main/java/com/blackducksoftware/integration/fortify/service/FortifyFileTokenApi.java
@@ -46,8 +46,8 @@ public class FortifyFileTokenApi {
     private FortifyFileTokenApiService apiService;
 
     private void init() {
-        okBuilder = getHeader(env.getProperty("FORTIFY_USERNAME"), env.getProperty("FORTIFY_PASSWORD"));
-        retrofit = new Retrofit.Builder().baseUrl(env.getProperty("FORTIFY_SERVER_URL")).addConverterFactory(GsonConverterFactory.create())
+        okBuilder = getHeader(env.getProperty("fortify.username"), env.getProperty("fortify.password"));
+        retrofit = new Retrofit.Builder().baseUrl(env.getProperty("fortify.server.url")).addConverterFactory(GsonConverterFactory.create())
                 .client(okBuilder.build()).build();
         apiService = retrofit.create(FortifyFileTokenApiService.class);
     }
