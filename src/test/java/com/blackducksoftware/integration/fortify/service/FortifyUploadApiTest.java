f0b61678f735b163c2251f60d8d1da071f666f75
Senthil Murugan Manikantan <smanikantan@blackducksoftware.com>
2017-10-05 10:39:09 -0400

    Updated the upload api test to create the fortify application id on the fly instead of hardcoding it
    Updated the upload api test to create the fortify application id on the
    fly instead of hard coding it

--- src/test/java/com/blackducksoftware/integration/fortify/service/FortifyUploadApiTest.java
+++ src/test/java/com/blackducksoftware/integration/fortify/service/FortifyUploadApiTest.java
@@ -23,6 +23,8 @@
 package com.blackducksoftware.integration.fortify.service;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -32,12 +34,17 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.blackducksoftware.integration.exception.IntegrationException;
 import com.blackducksoftware.integration.fortify.batch.BatchSchedulerConfig;
 import com.blackducksoftware.integration.fortify.batch.TestApplication;
 import com.blackducksoftware.integration.fortify.batch.job.BlackDuckFortifyJobConfig;
 import com.blackducksoftware.integration.fortify.batch.job.SpringConfiguration;
+import com.blackducksoftware.integration.fortify.batch.util.MappingParser;
 import com.blackducksoftware.integration.fortify.batch.util.PropertyConstants;
+import com.blackducksoftware.integration.fortify.model.CommitFortifyApplicationRequest;
+import com.blackducksoftware.integration.fortify.model.CreateApplicationRequest;
 import com.blackducksoftware.integration.fortify.model.FileToken;
+import com.blackducksoftware.integration.fortify.model.UpdateFortifyApplicationAttributesRequest;
 
 import junit.framework.TestCase;
 
@@ -52,23 +59,67 @@ public class FortifyUploadApiTest extends TestCase {
     @Autowired
     private FortifyFileTokenApi fortifyFileTokenApi;
 
+    @Autowired
+    private FortifyApplicationVersionApi fortifyApplicationVersionApi;
+
+    @Autowired
+    private MappingParser mappingParser;
+
     @Test
     public void uploadCSVFile() throws Exception {
-        System.out.println("Executing uploadCSVFile");
-        FileToken fileToken = new FileToken("UPLOAD");
+        int id = 0;
+        try {
+            CreateApplicationRequest createApplicationRequest = createApplicationVersionRequest("Fortify-Test", "1.0");
+            id = fortifyApplicationVersionApi.createApplicationVersion(createApplicationRequest);
+            assertNotNull(id);
+            updateApplicationAttributesTest(id);
+            commitApplicationVersion(id);
 
-        String fileTokenResponse = fortifyFileTokenApi.getFileToken(fileToken);
-        System.out.println("File Token::" + fileTokenResponse);
-        Assert.assertNotNull(fileTokenResponse);
+            System.out.println("Executing uploadCSVFile");
+            FileToken fileToken = new FileToken("UPLOAD");
 
-        File file = new File("sample.csv");
-        // File file = Mockito.mock(File.class);
-        // File file = new
-        // File("/Users/smanikantan/Documents/hub-fortify-integration/report/solrWar2_4.10.4_20170510160506866.csv");
-        System.out.println("file::" + file);
+            String fileTokenResponse = fortifyFileTokenApi.getFileToken(fileToken);
+            System.out.println("File Token::" + fileTokenResponse);
+            Assert.assertNotNull(fileTokenResponse);
 
-        boolean response = fortifyUploadApi.uploadVulnerabilityByProjectVersion(fileTokenResponse, 2l, file);
-        System.out.println("uploadVulnerabilityResponse::" + response);
-        Assert.assertTrue(response);
+            File file = new File("sample.csv");
+            // File file = Mockito.mock(File.class);
+            // File file = new
+            // File("/Users/smanikantan/Documents/hub-fortify-integration/report/solrWar2_4.10.4_20170510160506866.csv");
+            System.out.println("file::" + file);
+
+            boolean response = fortifyUploadApi.uploadVulnerabilityByProjectVersion(fileTokenResponse, id, file);
+            System.out.println("uploadVulnerabilityResponse::" + response);
+            Assert.assertTrue(response);
+        } catch (IOException e) {
+            throw new IOException(e);
+        } catch (IntegrationException e) {
+            throw new IntegrationException(e);
+        } finally {
+            deleteApplicationVersion(id);
+        }
+    }
+
+    public void updateApplicationAttributesTest(int parentId) throws IOException, IntegrationException {
+        System.out.println("Executing updateApplicationAttributesTest");
+        List<UpdateFortifyApplicationAttributesRequest> request = mappingParser.addCustomAttributes();
+        fortifyApplicationVersionApi.updateApplicationAttributes(parentId, request);
+    }
+
+    public void commitApplicationVersion(int applicationId) throws IOException, IntegrationException {
+        System.out.println("Executing commitApplicationVersion");
+        CommitFortifyApplicationRequest request = new CommitFortifyApplicationRequest(true);
+        fortifyApplicationVersionApi.commitApplicationVersion(applicationId, request);
+    }
+
+    public void deleteApplicationVersion(int applicationId) throws IOException, IntegrationException {
+        System.out.println("Executing deleteApplicationVersion");
+        fortifyApplicationVersionApi.deleteApplicationVersion(applicationId);
+    }
+
+    private CreateApplicationRequest createApplicationVersionRequest(String fortifyProjectName, String fortifyProjectVersion) {
+        String TEMPLATE = "Prioritized-HighRisk-Project-Template";
+        return new CreateApplicationRequest(fortifyProjectVersion, "Built using API", true, false,
+                new CreateApplicationRequest.Project("", fortifyProjectName, "Built using API", TEMPLATE), TEMPLATE);
     }
 }
